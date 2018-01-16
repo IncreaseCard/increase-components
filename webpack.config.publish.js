deb90277d49829f0959a35155ff03dd89718f4d6
Santiago Ocamica <santi6982@gmail.com>
2018-01-16 20:42:53 -0300

    Bundle before publishing

---
+++ webpack.config.publish.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'src/lib');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: path.resolve(SRC_DIR, 'index.js'),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: (loader) => [
+                require('postcss-import')({ root: loader.resourcePath }),
+                require('postcss-cssnext')()
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+};
