2a7d278f069da239362aa9295461e7ae7e35c442
Santiago Ocamica <santi6982@gmail.com>
2018-04-16 14:34:41 -0300

    Removed console.log in Loading component

--- src/components/Loading/Loading.stories.js
+++ src/components/Loading/Loading.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Loading } from './Loading';
-console.log(Loading.propTypes);
 
 storiesOf('Loading')
   .add('normal', () => (
