253597bf88e08cf595ce9ac4c8005cf16ebb25b3
Santiago Ocamica <santi6982@gmail.com>
2018-05-15 12:53:24 -0300

    Fixed Currency component to be able to receive a language and currency.
    We needed to be able to pass it a language in to account for Chrome
    weird default (es-latam vs es-country).

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,18 +1,24 @@
 import PropTypes from 'prop-types';
 
-export default function Currency({ value }) {
+export default function Currency({ value, currency = 'ARS', language = navigator.language }) {
   if (isNaN(value)) {
     return null;
   }
   const options = {
     minimumFractionDigits: 2,
     style: 'currency',
-    currency: 'ARS'
-  }
-  const numberFormatter = new Intl.NumberFormat(navigator.language, options);
+    currency
+  };
+  const numberFormatter = new Intl.NumberFormat(language, options);
   return numberFormatter.format(value);
 }
 
 Currency.propTypes = {
-  value: PropTypes.number
+  value: PropTypes.number,
+  currency: PropTypes.string,
+  language: PropTypes.string
+};
+
+Currency.defaultProps = {
+  currency: 'ARS'
 };
