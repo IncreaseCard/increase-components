746176e5cda684f9b5002a188d4068a46181e453
Santiago Ocamica <santi6982@gmail.com>
2018-06-26 15:53:36 -0300

    Added Calendar export.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import "!css-loader!normalize.css";
+import '!css-loader!normalize.css';
 
-import "./styles/base.css";
+import './styles/base.css';
 
-import AmountBox from "./components/AmountBox/AmountBox";
-import Button from "./components/Button/Button";
-import Checkbox from "./components/Checkbox/Checkbox";
-import Currency from "./components/Currency/Currency";
-import Heading from "./components/Heading/Heading";
-import Link from "./components/Link/Link";
-import Loading from "./components/Loading/Loading";
-import MessageList from "./components/MessageList/MessageList";
-import NumberInput from "./components/NumberInput/NumberInput";
-import Pagination from "./components/Pagination/Pagination";
-import PaginationButton from "./components/PaginationButton/PaginationButton";
-import Panel from "./components/Panel/Panel";
-import RadioButton from "./components/RadioButton/RadioButton";
-import Select from "./components/Select/Select";
-import SelectItem from "./components/SelectItem/SelectItem";
-import StepList from "./components/StepList/StepList";
-import Tab from "./components/Tab/Tab";
-import TabGroup from "./components/TabGroup/TabGroup";
-import TextInput from "./components/TextInput/TextInput";
+import AmountBox from './components/AmountBox/AmountBox';
+import Button from './components/Button/Button';
+import Calendar from './components/Calendar/Calendar';
+import Checkbox from './components/Checkbox/Checkbox';
+import Currency from './components/Currency/Currency';
+import Heading from './components/Heading/Heading';
+import Link from './components/Link/Link';
+import Loading from './components/Loading/Loading';
+import MessageList from './components/MessageList/MessageList';
+import NumberInput from './components/NumberInput/NumberInput';
+import Pagination from './components/Pagination/Pagination';
+import PaginationButton from './components/PaginationButton/PaginationButton';
+import Panel from './components/Panel/Panel';
+import RadioButton from './components/RadioButton/RadioButton';
+import Select from './components/Select/Select';
+import SelectItem from './components/SelectItem/SelectItem';
+import StepList from './components/StepList/StepList';
+import Tab from './components/Tab/Tab';
+import TabGroup from './components/TabGroup/TabGroup';
+import TextInput from './components/TextInput/TextInput';
 
 export {
   AmountBox,
   Button,
+  Calendar,
   Checkbox,
   Currency,
   Heading,
