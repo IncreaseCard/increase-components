6dcd21aa9827d2c6761baf8ea423cbb69cd597fd
Santiago Ocamica <santi6982@gmail.com>
2018-06-27 14:06:16 -0300

    Fixed calendar locale.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import moment from "moment";
-import { extendMoment } from "moment-range";
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { extendMoment } from 'moment-range';
 
-import CalendarWrapper from "./CalendarWrapper";
-import CalendarHeader from "./CalendarHeader";
-import CalendarDay from "./CalendarDay";
-import DayNumber from "./DayNumber";
+import CalendarWrapper from './CalendarWrapper';
+import CalendarHeader from './CalendarHeader';
+import CalendarDay from './CalendarDay';
+import DayNumber from './DayNumber';
 
 const propTypes = {
   renderDay: PropTypes.func,
@@ -14,35 +14,32 @@ const propTypes = {
   year: PropTypes.number
 };
 
+moment.defineLocale('es-AR', {
+  parentLocale: 'es',
+  week: {
+    dow: 0
+  }
+});
+moment.locale('es-AR');
+
 function Calendar({ renderDay, month, year }) {
-  moment.locale("es");
-  const startDate = moment({ month, year }).startOf("week");
+  const startDate = moment({ month, year }).startOf('week');
   const endDate = moment({ month, year })
-    .endOf("month")
-    .endOf("week");
+    .endOf('month')
+    .endOf('week');
   const momentExtended = extendMoment(moment);
-  const days = Array.from(momentExtended.range(startDate, endDate).by("days"));
+  const days = Array.from(momentExtended.range(startDate, endDate).by('days'));
   return (
     <CalendarWrapper>
-      {moment
-        .weekdays()
-        .map(weekday => (
-          <CalendarHeader key={weekday}>{weekday}</CalendarHeader>
-        ))}
-      {days.map(date => (
+      {moment.weekdays().map((weekday) => <CalendarHeader key={weekday}>{weekday}</CalendarHeader>)}
+      {days.map((date) => (
         <CalendarDay
           key={date}
-          today={date.isSame(moment(), "date")}
+          today={date.isSame(moment(), 'date')}
           inMonth={date.month() === month}
         >
-          <DayNumber today={date.isSame(moment(), "date")}>
-            {date.date()}
-          </DayNumber>
-          {renderDay(
-            date,
-            date.month() === month,
-            date.isSame(moment(), "date")
-          )}
+          <DayNumber today={date.isSame(moment(), 'date')}>{date.date()}</DayNumber>
+          {renderDay(date, date.month() === month, date.isSame(moment(), 'date'))}
         </CalendarDay>
       ))}
     </CalendarWrapper>
