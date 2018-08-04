6282ad585376490b0e2a3490d661823762c8dcf4
Santiago Ocamica <santi6982@gmail.com>
2018-08-04 18:03:39 -0300

    Fixed bundle configuration for proper library build

--- webpack.config.publish.js
+++ webpack.config.publish.js
@@ -11,9 +11,9 @@ module.exports = {
     path: DIST_DIR,
     filename: 'bundle.js',
     library: {
-      root: "IncreaseComponents",
-      amd: "increase-components",
-      commonjs: "increase-components"
+      root: 'IncreaseComponents',
+      amd: 'increase-components',
+      commonjs: 'increase-components'
     },
     libraryTarget: 'umd'
   },
@@ -28,12 +28,12 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'style-loader',
+            loader: 'style-loader'
           },
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1,
+              importLoaders: 1
             }
           },
           {
@@ -58,8 +58,43 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.NamedModulesPlugin()
-  ]
+  plugins: [new webpack.optimize.UglifyJsPlugin(), new webpack.NamedModulesPlugin()],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: '_'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: '_'
+    },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'prop-types',
+      root: '_'
+    },
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components',
+      root: '_'
+    },
+    moment: {
+      commonjs: 'moment',
+      commonjs2: 'moment',
+      amd: 'moment',
+      root: '_'
+    },
+    'moment-range': {
+      commonjs: 'moment-range',
+      commonjs2: 'moment-range',
+      amd: 'moment-range',
+      root: '_'
+    }
+  }
 };
