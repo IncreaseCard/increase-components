495d8fd4944e02800fd1dc895b25dda77e378158
Santiago Ocamica <santi6982@gmail.com>
2018-10-15 11:49:49 -0300

    Readded removed storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,24 @@
 import '!css-loader!normalize.css';
 import '../src/styles/base.css';
+import React from 'react';
+import { configure, addDecorator, setAddon } from '@storybook/react';
+import infoAddon from '@storybook/addon-info';
+import { checkA11y } from '@storybook/addon-a11y';
 
-import { configure } from '@storybook/react';
+addDecorator((story) => (
+  <div
+    style={{
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      padding: '16px'
+    }}
+  >
+    {story()}
+  </div>
+));
+addDecorator(checkA11y);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
