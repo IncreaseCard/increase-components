4b4ed839d1b0ac9d6b16c3da5fe3e3ee3b817518
Santiago Ocamica <santi6982@gmail.com>
2018-10-16 14:14:11 -0300

    Fixed TabGroup vertical padding

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -14,12 +14,13 @@ export const Tab = styled.button`
       return `color: ${colors.brand.blue[500]};`;
     }
   }}
+  box-sizing: border-box;
   background-color: ${(props) => props.theme.colors.white[100]};
   border: none;
   ${(props) => (props.disabled ? null : `cursor: pointer;`)}
   font-size: ${(props) => props.theme.typography.bodyFontSizes[0]};
   line-height: ${(props) => props.theme.typography.bodyLineHeights[0]};
-  padding: 4px 20px;
+  padding: 10px 20px;
 
   &:hover:not(:disabled) {
     color: ${(props) => props.theme.colors.lightBlue[500]};
