b12cf0901f0c280ba9d220042da07079bd29e76c
Santiago Ocamica <santi6982@gmail.com>
2018-10-18 11:06:35 -0300

    Fixed Tabs baseline text alignment due to bottom border.

--- src/components/TabGroup/Tab.js
+++ src/components/TabGroup/Tab.js
@@ -1,10 +1,30 @@
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import newTheme from '../../themes/new';
 
 export const Tab = styled.button`
-  ${(props) => {
+  box-sizing: border-box;
+  background: transparent;
+  border: none;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid
+    ${(props) => (props.active ? props.theme.colors.lightBlue[500] : 'transparent')};
+  font-size: ${(props) => props.theme.typography.bodyFontSizes[0]};
+  line-height: ${(props) => props.theme.typography.bodyLineHeights[0]};
+  height: calc(100% + 1px); /* 1px correction for TabGroup bottom border */
+  margin-bottom: -1px;
+  padding: 0 20px;
+
+  &:hover:not(:disabled) {
+    color: ${(props) => props.theme.colors.lightBlue[500]};
+  }
+
+  * + & {
+    margin-left: 20px;
+  }
+
+  ${(props) => (props.disabled ? null : `cursor: pointer;`)} ${(props) => {
     const colors = props.theme.colors;
     if (props.disabled) {
       return `color: ${colors.gray[500]};`;
@@ -13,29 +33,7 @@ export const Tab = styled.button`
     } else {
       return `color: ${colors.brand.blue[500]};`;
     }
-  }}
-  box-sizing: border-box;
-  background: transparent;
-  border: none;
-  ${(props) => (props.disabled ? null : `cursor: pointer;`)}
-  font-size: ${(props) => props.theme.typography.bodyFontSizes[0]};
-  line-height: ${(props) => props.theme.typography.bodyLineHeights[0]};
-  height: 100%;
-  padding: 0 20px;
-
-  &:hover:not(:disabled) {
-    color: ${(props) => props.theme.colors.lightBlue[500]};
-  }
-  * + & {
-    margin-left: 20px;
-  }
-
-  ${(props) =>
-    props.active
-      ? css`
-          border-bottom: 2px solid ${props.theme.colors.lightBlue[500]};
-        `
-      : null}
+  }};
 `;
 
 Tab.propTypes = {
