8b955151096080b074c1b3443e1d5256f7cd4b55
Santiago Ocamica <santi6982@gmail.com>
2019-03-27 11:56:27 -0300

    Added calendar dominicana story

--- src/components/Calendar/Calendar.stories.js
+++ src/components/Calendar/Calendar.stories.js
@@ -74,4 +74,45 @@ storiesOf('Calendar', module)
         </div>
       </div>
     );
+  })
+  .add('dominicana', () => {
+    const CalendarData = styled.div`
+      ${(props) => (!props.inMonth ? 'opacity: 0.5' : null)};
+      margin-bottom: 1px;
+      padding: 4px;
+    `;
+    const renderDay = (date, inMonth) => (
+      <React.Fragment>
+        <CalendarData inMonth={inMonth} style={{ color: '#f79c31' }}>
+          <Currency currency="DOP" language="es-DO" value={200 * Math.random()} />
+        </CalendarData>
+        <CalendarData inMonth={inMonth} style={{ color: '#2ca8df' }}>
+          <Currency currency="DOP" language="es-DO" value={250 * Math.random()} />
+        </CalendarData>
+      </React.Fragment>
+    );
+
+    const renderWeeklySummary = () => (
+      <React.Fragment>
+        <CalendarData inMonth={true} style={{ color: '#f79c31' }}>
+          <Currency currency="DOP" language="es-DO" value={7 * 200 * Math.random()} />
+        </CalendarData>
+        <CalendarData inMonth={true} style={{ color: '#2ca8df' }}>
+          <Currency currency="DOP" language="es-DO" value={5 * 250 * Math.random()} />
+        </CalendarData>
+      </React.Fragment>
+    );
+    return (
+      <div style={{ backgroundColor: '#FAFAFA', padding: '8px' }}>
+        <div style={{ width: '90%', margin: '0 auto' }}>
+          <Calendar
+            hasWeeklySummary={true}
+            month={5}
+            renderDay={renderDay}
+            renderWeeklySummary={renderWeeklySummary}
+            year={2018}
+          />
+        </div>
+      </div>
+    );
   });
