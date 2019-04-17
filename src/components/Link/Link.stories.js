6b4d002ea67f24a9a01c709a5e20c4b64f32941d
Sebastian zelonka <sebastian.zelonka@increasecard.com>
2019-04-17 11:21:21 -0300

    Added avility to use target="blank" on Link

--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -4,17 +4,30 @@ import { storiesOf } from '@storybook/react';
 
 import Link from './Link';
 
-storiesOf('Link', module).add('to a normal url', () => (
-  <>
-    <Link href="https://www.google.com.ar">www.google.com.ar</Link>
-    <Link disabled href="https://www.google.com.ar">
+storiesOf('Link', module)
+  .add('Regular', () => (
+    <>
+      <Link href="https://www.google.com.ar">www.google.com.ar</Link>
+      <br />
+      <Link disabled href="https://www.google.com.ar">
+        www.google.com.ar
+      </Link>
+      <br />
+    </>
+  ))
+  .add('Alert', () => (
+    <>
+      <Link href="https://www.google.com.ar" variant="alert">
+        www.google.com.ar
+      </Link>
+      <br />
+      <Link disabled href="https://www.google.com.ar" variant="alert">
+        www.google.com.ar
+      </Link>
+    </>
+  ))
+  .add('Target="blank"', () => (
+    <Link href="https://www.google.com.ar" target="blank">
       www.google.com.ar
     </Link>
-    <Link href="https://www.google.com.ar" variant="alert">
-      www.google.com.ar
-    </Link>
-    <Link disabled href="https://www.google.com.ar" variant="alert">
-      www.google.com.ar
-    </Link>
-  </>
-));
+  ));
