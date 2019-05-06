eb010c63fe4ac706ae6ebbd8befcff8db01aa67d
Sebastian zelonka <me@sebazelonka.com>
2019-05-06 10:48:34 -0300

    changed padding by px value + changed caret from className to styled component to prevent collision of styles

--- src/components/ApplicationHeader/ApplicationHeader.js
+++ src/components/ApplicationHeader/ApplicationHeader.js
@@ -12,8 +12,9 @@ const ApplicationHeaderWrapper = styled.div`
   z-index: 10;
   border-bottom: 1px solid ${(props) => props.theme.colors.whiteTone};
   padding: 10px;
+  font-size: 15px;
   @media (min-width: ${(props) => props.theme.breakpoints.medium}) {
-    padding: 1rem;
+    padding: 15px;
   }
 `;
 
@@ -49,11 +50,6 @@ const LogoWrapper = styled.div`
     font-weight: 600;
     margin: 0 9px 0 12px;
   }
-  .caret {
-    height: 100%;
-    width: 100%;
-    margin-top: 3px;
-  }
 `;
 
 LogoWrapper.defaultProps = { theme: NewTheme };
@@ -62,8 +58,7 @@ LogoWrapper.displayName = 'LogoWrapper';
 const RightContent = styled.div`
   display: flex;
   align-items: center;
-  .alerts,
-  .country {
+  .alerts {
     margin-right: 1rem;
     padding-right: 1rem;
     border-right: 1px solid ${(props) => props.theme.colors.whiteTone};
@@ -71,17 +66,16 @@ const RightContent = styled.div`
   .alerts {
     color: ${(props) => props.theme.colors.whiteTone};
   }
-  .country {
-    display: flex;
-    .caret {
-      margin-left: 0.75rem;
-      color: ${(props) => props.theme.colors.brandBlueShade};
-    }
-  }
 `;
 
 RightContent.defaultProps = { theme: NewTheme };
 RightContent.displayName = 'RightContent';
+
+const StyledCaret = styled(Icon)`
+  height: 100%;
+  width: 100%;
+  margin-top: 3px;
+`;
 
 export default function ApplicationHeader({ country, currentProduct, products, user }) {
   const [isOpen, setOpen] = useState(false);
@@ -97,9 +91,9 @@ export default function ApplicationHeader({ country, currentProduct, products, u
               <Icon className="logo" src={LogoVerde} />
               <span className="brandName">Increase</span>
               {isOpen ? (
-                <Icon className="caret" src={ArrowDown} />
+                <StyledCaret src={ArrowDown} />
               ) : (
-                <Icon className="caret" src={ArrowUp} />
+                <StyledCaret src={ArrowUp} />
               )}
             </LogoWrapper>
           </div>
